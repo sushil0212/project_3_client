e64b977950155f01775bc3c1b98c9dbd142e0395
“Sushil <sushilstar9@gmail.com>
2024-09-03 12:54:26 +0100

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,6 @@ import DetailsPage from "./pages/DetailsPage";
 import Notifications from "./pages/Notifications";
 import Dashboard from "./pages/Dashboard";
 import About from "./components/About";
-
 import SavedShorts from "./components/SavedShorts";
 
 function App() {
@@ -37,111 +36,110 @@ function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage />}
-        />
-        <Route
-          path="/about"
-          element={<About />}
-        />
-
-        <Route
-          path="/signup"
-          element={
-            <AnonRoute>
-              <Signup />
-            </AnonRoute>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <AnonRoute>
-              <Login />
-            </AnonRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={<ProfilePage />}
-        />
-        <Route
-          path="/activity"
-          element={<Activity />}
-        />
-        <Route
-          path="/preview/:videoId"
-          element={<Preview />}
-        />
-        <Route
-          path="/vacancy-required/:jobId"
-          element={<VacancyRequired />}
-        />
-        <Route
-          path="/jobs-ads"
-          element={
-            <PrivateRoute>
-              <JobsAds />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/shorts"
-          element={
-            <PrivateRoute>
-              <Shorts />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/generate-video/:jobId"
-          element={<GenerateVideo />}
-        />
-        <Route
-          path="/shorts/:shortId"
-          element={
-            <PrivateRoute>
-              <DetailsPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/payment"
-          element={
-            <PrivateRoute>
-              <Payment />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/notifications"
-          element={
-            <PrivateRoute>
-              <Notifications />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/saved-shorts"
-          element={<SavedShorts />}
-        />
-      </Routes>
-
+      {/* Adjust padding-top to avoid content being hidden behind the fixed Navbar */}
       {showNavbar && <Navbar />}
+      <div className={`content-wrapper ${showNavbar ? "pt-16" : ""}`}>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/signup"
+            element={
+              <AnonRoute>
+                <Signup />
+              </AnonRoute>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <AnonRoute>
+                <Login />
+              </AnonRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={<ProfilePage />}
+          />
+          <Route
+            path="/activity"
+            element={<Activity />}
+          />
+          <Route
+            path="/preview/:videoId"
+            element={<Preview />}
+          />
+          <Route
+            path="/vacancy-required/:jobId"
+            element={<VacancyRequired />}
+          />
+          <Route
+            path="/jobs-ads"
+            element={
+              <PrivateRoute>
+                <JobsAds />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/shorts"
+            element={
+              <PrivateRoute>
+                <Shorts />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/generate-video/:jobId"
+            element={<GenerateVideo />}
+          />
+          <Route
+            path="/shorts/:shortId"
+            element={
+              <PrivateRoute>
+                <DetailsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/payment"
+            element={
+              <PrivateRoute>
+                <Payment />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/notifications"
+            element={
+              <PrivateRoute>
+                <Notifications />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/saved-shorts"
+            element={<SavedShorts />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
 
 export default App;
- 
